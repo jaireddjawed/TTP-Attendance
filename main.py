bbe990a9da61dfc4a182b4a50043f62a3b1bd595
Jaired Jawed <jairedjawed@icloud.com>
2022-02-09 08:51:08 -0800

    Allow students to sign in via card swipe

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def clearTerminal():
   system('cls' if path.exists('cls') else 'clear')
 
 def addStudentManually(spreadsheetId, service):
+  # todo: validate the user's input
   firstName = input('Please enter your first name: ')
   lastName = input('Please enter your last name: ')
-  studentId = input('Please enter your student ID: ')
+  studentId = input('Please enter your SID: ')
 
   service.spreadsheets().values().append(
     spreadsheetId=spreadsheetId,
@@ -31,7 +32,38 @@ def addStudentManually(spreadsheetId, service):
   clearTerminal()
   sleep(0.5)
   print('Successfully signed in.')
-  sleep(2)
+  sleep(0.5)
+  clearTerminal()
+
+def parseCardSwipeInput(card):
+  fullName = card.split('^')[1].split('/')
+
+  # first and last name are in reverse order
+  firstName = fullName[1]
+  lastName = fullName[0]
+
+  return firstName, lastName
+
+def addStudentCardSwipe(spreadsheetId, service, firstName, lastName):
+  service.spreadsheets().values().append(
+    spreadsheetId=spreadsheetId,
+    range='Sheet1!A1:E100',
+    valueInputOption='USER_ENTERED',
+    body={
+      'values': [[
+        firstName,
+        lastName,
+        '', # no student id is provided from the card swipe
+        date.today().strftime("%m/%d/%Y"),
+        datetime.today().strftime("%I:%M %p")
+      ]]
+    }
+  ).execute()
+
+  clearTerminal()
+  sleep(0.5)
+  print('Successfully signed in.')
+  sleep(0.5)
   clearTerminal()
 
 def main():
@@ -43,10 +75,18 @@ def main():
 
   while True:
     print('Welcome to the TTP Attendance Tracker.')
-    print('Please swipe your card or press "Enter" to add details manually.')
-    print("By signing-in you acknowledge that you have read and agree to abide by the Engineering Transfer Center (WCH 103) Space Policies.\n")
+    print('By signing-in you acknowledge that you have read and agree to abide by the Engineering Transfer Center (WCH 103) Space Policies.\n')
 
-    addStudentManually(sheet['spreadsheetId'], service)
+    choice = input('Please swipe your card or press "m" and "Enter" to add details manually: ')
+
+    if choice == 'm':
+      addStudentManually(sheet['spreadsheetId'], service)
+    elif choice == 'q':
+      break
+    else:
+      # get the first name and last name of student from card swipe
+      firstName, lastName = parseCardSwipeInput(choice)
+      addStudentCardSwipe(sheet['spreadsheetId'], service, firstName, lastName)
 
 if __name__ == '__main__':
   main()
