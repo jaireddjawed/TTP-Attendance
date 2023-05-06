4cfe70ec1ac8dea18ea8a29743e04b9e9e38c01a
Jaired Jawed <jairedjawed@icloud.com>
2023-05-05 18:29:18 -0700

    created signin page.

---
+++ static/js/signin.js
@@ -0,0 +1,118 @@
+/*
+  * This function submits the sign in information to the server
+  * and redirects the user to the success page if the sign in was successful.
+*/
+
+function submitSignInInformation(firstName, lastName, studentId) {
+  fetch('/submit-signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'first-name': firstName,
+      'last-name': lastName,
+      'student-id': studentId,
+    })
+  })
+  .then(response => {
+    if (!response.ok) {
+      alert('There was an error signing you in. Please try again.');
+    }
+    else {
+      window.location.href = '/success';
+    }
+  });
+}
+
+/*
+ * form validation to ensure that everyone
+ * fills out every field before submitting
+ * the sign in form
+ */
+
+$(document).ready(function() {
+  $('.form').validate({
+    rules: {
+      'first-name': {
+        required: true,
+      },
+      'last-name': {
+        required: true,
+      },
+      'student-id': {
+        required: true,
+      },
+    },
+    messages: {
+      'first-name': {
+        required: 'Please enter your first name.',
+      },
+      'last-name': {
+        required: 'Please enter your last name.',
+      },
+      'student-id': {
+        required: 'Please enter your student id.',
+      },
+    },
+    submitHandler: function(form) {
+      submitSignInInformation(
+        document.querySelector('input[name="first-name"]').value,
+        document.querySelector('input[name="last-name"]').value,
+        document.querySelector('input[name="student-id"]').value,
+      );
+    },
+  });
+});
+
+/*
+ * This event listener is for the id card reader. Once a student swipes their id card, it parses their
+ * first and last name from the id card string and submits the sign in form.
+ */
+document.querySelector('input[name="id-card-reader"]').addEventListener('keydown', (event) => {
+  let fullName;
+
+  try {
+    const idString = document.querySelector('input[name="id-card-reader"]').value;
+    fullName = idString.split('^')[1].split('/');
+  } catch (error) {
+    return;
+  }
+
+  const [, lastName, firstName] = fullName;
+  submitSignInInformation(firstName, lastName, '');
+});
+
+/*
+ * Inactivity needs to be measured in case someone clicks on one of the inputs and
+ * doesn't sign in so that the browser can refocus on the id-card input in case
+ * the next student signs in with their id card.
+ * 
+ * If it does not refocus on the id card input, no one will be able to sign in with their student id.
+ */
+
+let inactivity = 0;
+
+addEventListener('keypress', () => {
+  inactivity = 0;
+});
+
+addEventListener('mousemove', () => {
+  inactivity = 0;
+});
+
+addEventListener('click', () => {
+  inactivity = 0;
+});
+
+// measure inactivity every second
+setInterval(() => {
+  inactivity++;
+
+  // refocus after 15 seconds of inactivity`
+  if (inactivity >= 15) {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(input => input.value = '');
+    document.querySelector('input[name="id-card-reader"]').focus();
+  }
+}, 1000);
