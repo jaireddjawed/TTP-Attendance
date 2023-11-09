68d2faf2b5a3b5bed81a4003962a8fd9e9382200
Jaired Jawed <jjawe001@ucr.edu>
2023-11-09 15:04:16 -0800

    changed commands for run-macos and run-windows

--- run-macos.sh
+++ run-macos.sh
@@ -1,4 +1,4 @@
 export FLASK_APP=app.py
 export FLASK_ENV=production
 export FLASK_DEBUG=0
-flask run
+python -m flask run
--- run-windows.sh
+++ run-windows.sh
@@ -1,4 +1,4 @@
 set FLASK_APP=app.py
 set FLASK_ENV=production
 export FLASK_DEBUG=0
-flask run
+python -m flask run
